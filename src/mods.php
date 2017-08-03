9ef125b8bb7708cc171b7427ee80230bfd2925a4
Alexandre Oliveira <rockytvbr@gmail.com>
2017-08-03 16:56:08 -0300

    Add route for random mod

--- src/mods.php
+++ src/mods.php
@@ -6,4 +6,13 @@ $app->get('/mod/{id}[/{name}]', function($request, $response, $args) {
     ]);
 })->setName('mod.view');
 
+$app->get('/random', function($request, $response, $args) {
+    $backend_url = $this->get('settings')['backend-url'];
+    $gameshort = $this->get('settings')['gameshort'];
+    $json = file_get_contents('http://'  . $backend_url . '/api/mods/' . $gameshort . '/random');
+    $random_mod = json_decode($json);
+
+    return $response->withRedirec('/mod/ '.$random_mod->data[0]->id);
+})->setName('mod.random');
+
 ?>
