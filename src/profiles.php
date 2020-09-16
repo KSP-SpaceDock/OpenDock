8d0c5acad4b70c89c62821ee0983efecff98ca7d
Alexandre Oliveira <rockytvbr@gmail.com>
2020-09-16 21:15:17 +0200

    Cleanup files

--- src/profiles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$app->get('/profile/{id}[/{name}]', function($request, $response, $args) {
-    return $this->view->render($response, 'templates/view_profile.html', [
-        'userid' => $args['id']
-    ]);
-})->setName('profile.view');
-
-?>
